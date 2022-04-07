ce918a3ea33a272603011a6dcf4ad3a58ce838bb
inbalcastel <97899250+inbalcastel@users.noreply.github.com>
2022-04-07 10:12:51 +0000

    fix

--- src/App.js
+++ src/App.js
@@ -5,46 +5,44 @@ function App() {
 
   const audioNames = [{
     id:0,
-    title:  "s3-us1",
-},
+    title:  "s3-us1"},
 {
     id:1,
-    title:   "s3-us2",
+    title:   "s3-us2"
 },
 {
     id:2,
-    title:   "src_assets_loops_1-GHS_123_Filo_Kick_Clap",
+    title:   "src_assets_loops_1-GHS_123_Filo_Kick_Clap"
 },
 {
     id:3,
-    title:   "src_assets_loops_3-GHS_123_Gm_Sun_Synth_Bass",
+    title:   "src_assets_loops_3-GHS_123_Gm_Sun_Synth_Bass"
 
 },
 {
   id:4,
-  title:   "src_assets_loops_5-hk_gtr125_pickcut_Gm",
-
+  title:   "src_assets_loops_5-hk_gtr125_pickcut_Gm"
 },
 {
   id:5,
-  title:   "src_assets_loops_6-hk_syn125_holdme1_Gm",
+  title:   "src_assets_loops_6-hk_syn125_holdme1_Gm"
 
 },
 {
   id:6,
-  title:   "src_assets_loops_7-hk_top125_latint",
+  title:   "src_assets_loops_7-hk_top125_latint"
 
 },
 ,
 {
   id:7,
-  title:   "src_assets_loops_8-hk_top125_zulu",
+  title:   "src_assets_loops_8-hk_top125_zulu"
 
 },
 ,
 {
   id:8,
-  title:   "src_assets_loops_9-hk_mus125_lovefunk2_Gm",
+  title:   "src_assets_loops_9-hk_mus125_lovefunk2_Gm"
 
 }
   ]
